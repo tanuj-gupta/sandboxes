3a70addfaac280774048ba387d10fa14164d5b3f
Gupta <tgupta@ezesoft.com>
2017-12-14 13:09:20 +0530

    angular 2 course follow-along files

---
+++ angular/APM/src/app/products/product.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './product';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+import { HttpErrorResponse } from '@angular/common/http/src/response';
+
+@Injectable()
+export class ProductService {
+
+    constructor(private _http: HttpClient) {}
+
+    private _productUrl = './api/products/products.json';
+    getProducts(): Observable<IProduct[]> {
+        return this._http.get<IProduct[]>(this._productUrl)
+                .do(data => console.log('All: ' + JSON.stringify(data)))
+                .catch(this.handleError);
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        console.log(err.message);
+        return Observable.throw(err.message);
+    }
+}
